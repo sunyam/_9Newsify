6c5f3a4dc1a08c3111aef12086b00557bf9874e5
sunyam <sunyambagga@gmail.com>
2016-03-03 13:44:08 +0530

    HomePage complete

--- home_page/models.py
+++ home_page/models.py
@@ -3,7 +3,3 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=100)
-    email = models.EmailField()
-    password = models.CharField(max_length=20)   # Subject to Change!
