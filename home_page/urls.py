5a30cf157e33537b360bf5efd660aab7dfc3580c
sunyam <sunyambagga@gmail.com>
2016-04-05 18:24:17 +0530

    fixing background-image bug and minor changes

--- home_page/urls.py
+++ home_page/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
         url('^$', views.home, name='home'),
+        url('^login', views.login, name='login')
     ]
--- home_page/views.py
+++ home_page/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home_page/home.html')
+
+def login(request):
+    return render(request, 'home_page/login.html')
